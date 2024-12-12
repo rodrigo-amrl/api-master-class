da6b7888fbe5c7331974d5c05f25ea84303df61e
Rodrigo Amaral <rodrigo.amaral94@hotmail.com>
2024-12-11 21:24:24 -0300

    resources

--- app/Http/Controllers/Api/V1/AuthorTicketController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketController.php
@@ -4,14 +4,49 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Filters\V1\TicketFilter;
+use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Resources\Api\V1\TicketResource;
 use App\Models\Ticket;
-use Illuminate\Http\Request;
+use App\Models\User;
+use App\Traits\ApiResponses;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class AuthorTicketController extends Controller
 {
+    use ApiResponses;
     public function index($author_id, TicketFilter $filters)
     {
         return TicketResource::collection(Ticket::where('user_id', $author_id)->filter($filters)->paginate());
+    }
+    public function store($author_id, StoreTicketRequest $request)
+    {
+        try {
+            User::findOrFail($author_id);
+        } catch (ModelNotFoundException $e) {
+            return $this->ok('User not Found', [
+                'error' => "The provided user id does not exists"
+            ]);
+        }
+
+        $model = [
+            'title' => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status' => $request->input('data.attributes.status'),
+            'user_id' => $author_id
+        ];
+        return TicketResource::make(Ticket::create($model));
+    }
+    public function destroy(int $author_id, int $ticket_id)
+    {
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+            if ($ticket->user_id == $author_id) {
+                $ticket->delete();
+                return $this->ok('Ticket deleted');
+            }
+            return $this->error('Ticket not Found', 404);
+        } catch (ModelNotFoundException $e) {
+            return $this->error('Ticket cannot be Found', 404);
+        }
     }
 }
--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -7,21 +7,58 @@ use App\Models\Ticket;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Requests\Api\V1\UpdateTicketRequest;
 use App\Http\Resources\Api\V1\TicketResource;
+use App\Models\User;
+use App\Traits\ApiResponses;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class TicketController extends ApiController
 {
+    use ApiResponses;
+
     public function index(TicketFilter $filters)
     {
         return TicketResource::collection(Ticket::filter($filters)->paginate());
     }
-    public function store(StoreTicketRequest $request) {}
-    public function show(Ticket $ticket)
+    public function store(StoreTicketRequest $request)
     {
-        if ($this->include('author')) {
-            return new TicketResource($ticket->load('user'));
+        try {
+            $user = User::findOrFail($request->input('data.relationships.author.data.id'));
+        } catch (ModelNotFoundException $e) {
+            return $this->ok('User not Found', [
+                'error' => "The provided user id does not exists"
+            ]);
         }
-        return new TicketResource($ticket);
+
+        $model = [
+            'title' => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status' => $request->input('data.attributes.status'),
+            'user_id' => $user->id
+        ];
+        return TicketResource::make(Ticket::create($model));
+    }
+    public function show(int $ticket_id)
+    {
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+            if ($this->include('author')) {
+                return new TicketResource($ticket->load('user'));
+            }
+            return new TicketResource($ticket);
+        } catch (ModelNotFoundException $e) {
+            return $this->error('Ticket cannot be Found', 404);
+        }
     }
     public function update(UpdateTicketRequest $request, Ticket $ticket) {}
-    public function destroy(Ticket $ticket) {}
+    public function destroy(int $ticket_id)
+    {
+
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+            $ticket->delete();
+            return $this->ok('Ticket deleted');
+        } catch (ModelNotFoundException $e) {
+            return $this->error('Ticket cannot be Found', 404);
+        }
+    }
 }
